192ae5db88bf5cf9ba5101bffb62862f8f530cf6
Gerson Carvalho <gecarval@c2r10s8.42luanda.com>
2025-09-09 09:09:20 +0100

    fix(NeuralNetwork): for loop comparison and iterator type in train member function.

--- classes/NeuralNetwork.cpp
+++ classes/NeuralNetwork.cpp
@@ -1,7 +1,7 @@
 #include "NeuralNetwork.hpp"
 
 static float sigmoid(const float x) {
-	return (1 / (1 + std::exp(-x)));
+	return (1.0 / (1.0 + std::exp(-x)));
 }
 
 static float relu(const float x) {
@@ -130,7 +130,7 @@ void NeuralNetwork::train(const DMatrix &inputArray, const DMatrix &desired) {
 	const DMatrix  output(this->feedFoward(inputArray));
 	const DMatrix &target = desired;
 	DMatrix		   outputError((target - output));
-	for (size_t i = this->hiddenLayerLen; i != 0; i--) {
+	for (long i = this->hiddenLayerLen; i >= 0; i--) {
 		outputError = this->weight[i].transpose() * outputError;
 	}
 }
@@ -140,7 +140,7 @@ void NeuralNetwork::train(const std::vector<float> &inputArray,
 	const DMatrix output(this->feedFoward(inputArray));
 	const DMatrix target(desired);
 	DMatrix		  outputError((target - output));
-	for (size_t i = this->hiddenLayerLen; i != 0; i--) {
+	for (long i = this->hiddenLayerLen; i >= 0; i--) {
 		outputError = this->weight[i].transpose() * outputError;
 	}
 }
