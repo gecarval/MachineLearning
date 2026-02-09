c681b3b78d18e5985d85a696c83482c17488d3cb
kazekura <gersonrcarvalho77@gmail.com>
2026-02-09 19:01:37 +0100

    feat(MainMenu): added buttons to main menu.

--- srcs/states/mainMenu.cpp
+++ srcs/states/mainMenu.cpp
@@ -1,23 +1,46 @@
 #include "../../includes/Machine.hpp"
 
-void renderMainMenu(Machine &machine) {
-	// Main Menu Title Setting
-	static const char title[] = "MAIN MENU";
-	static const char subTitle[] = "PRESS ENTER or SPACE to START";
-	// Main Menu Title Padding
+void renderMainMenu(Machine &mch) {
+	// Main Menu Title Text
+	static std::string title = "MAIN MENU";
+	// Main Menu Title Position
 	const int	  titleFontSize = GetScreenWidth() / 20;
-	const int	  subTitleFontSize = titleFontSize / 2;
-	const int	  titlePosOffset = titleFontSize * sizeof(title) / 3;
+	const int	  titlePosOffset = titleFontSize * title.length() / 3;
 	const Vector2 screenCenter =
 		(Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
 	const Vector2 titlePos = (Vector2){screenCenter.x - titlePosOffset, 20};
-	const int	  subTitlePosOffset = subTitleFontSize * sizeof(subTitle) / 3;
-	const Vector2 subTitlePos =
-		(Vector2){screenCenter.x - subTitlePosOffset, screenCenter.y};
+	// Main Menu Button Position
+	const Vector2 buttonSize = (Vector2){200, 50};
+	const Vector2 buttonPos = screenCenter - buttonSize / 2;
+	const int	  padding2 = buttonSize.y + 10;
+	const int	  padding3 = 2 * buttonSize.y + 20;
+	const int	  padding4 = 3 * buttonSize.y + 30;
+	static Button button1(buttonPos.x, buttonPos.y, buttonSize, "Perceptron");
+	static Button button2(buttonPos.x, buttonPos.y + padding2, buttonSize,
+						  "Neural Network");
+	static Button button3(buttonPos.x, buttonPos.y + padding3, buttonSize,
+						  "Settings");
+	static Button button4(buttonPos.x, buttonPos.y + padding4, buttonSize,
+						  "Exit");
+	button1.setOnClick([&mch]() { mch.state = STATE::GAME::PERCEPTRON; });
+	button2.setOnClick([&mch]() { mch.state = STATE::GAME::NEURALNETWORK; });
+	button3.setOnClick([&mch]() { mch.state = STATE::MENU::SETTING; });
+	button4.setOnClick([&mch]() {
+		mch.points.clear();
+		rlImGuiShutdown();
+		CloseWindow();
+		std::exit(0);
+	});
+	button1.update();
+	button2.update();
+	button3.update();
+	button4.update();
 	ClearBackground(RAYWHITE);
-	DrawText(title, titlePos.x, titlePos.y, titleFontSize, DARKGREEN);
-	DrawText(subTitle, subTitlePos.x, subTitlePos.y, subTitleFontSize,
-			 DARKGREEN);
+	button1.draw();
+	button2.draw();
+	button3.draw();
+	button4.draw();
+	DrawText(title.c_str(), titlePos.x, titlePos.y, titleFontSize, DARKGREEN);
 }
 
 int handleMainState(Machine &machine) {
